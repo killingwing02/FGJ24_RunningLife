fd38e80e4f21d7c43fc65b5a027b0e753e504534
xykle <happy30102@gmail.com>
2024-08-24 21:03:41 +0800

    Import and update assets

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,36 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] private float jumpForce;
+    [SerializeField] private SpriteRenderer playerSprite;
 
     private Rigidbody2D rb;
-    private bool isJumped = false;
+    private Animator animator;
+    private bool isMidAir = false;
+    private bool isSliding = false;
     private bool isInvisablity = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && !isJumped)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !isMidAir)
         {
             rb.AddForce(jumpForce * Vector2.up);
-            isJumped = true;
+            animator.Play("Jump");
+            animator.SetBool("Jump", true);
+            //isMidAir = true;
             Debug.Log("Jumped!");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && !isSliding)
+        {
+            // Temp visual effect
+            //var scale = new Vector3(1f, .5f, 1f);
+            //transform.localScale = scale;
+
+            animator.SetBool("Slide", true);
+
+            isSliding = true;
+            Debug.Log("Sliding");
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow) && isSliding)
+        {
+            //transform.localScale = Vector3.one;
+
+            animator.SetBool("Slide", false);
+            isSliding = false;
+        }
+
+        // TODO: Not really working...
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+        if (hit.collider != null)
+        {
+            var color = Color.blue;
+            if (transform.position.y - hit.point.y > .755f && !isMidAir)
+            {
+                color = Color.green;
+                isMidAir = true;
+
+                if (!animator.GetBool("Jump"))
+                    animator.Play("Jump", 0, 6f / 11f);
+            }
+            else if (transform.position.y - hit.point.y <= .755f && isMidAir)
+            {
+                isMidAir = false;
+            }
+
+            Debug.DrawLine(transform.position, hit.point, color);
         }
 
         // Damage check
         if (isInvisablity) return;
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, .35f);
+        hit = Physics2D.Raycast(transform.position, Vector2.right, .35f);
         if (hit.collider != null && hit.collider.CompareTag("Blocker"))
         {
             Debug.Log("<color=red>Hit</color>: " + hit.collider.name);
+            isInvisablity = true;
+            LeanTween.alpha(playerSprite.gameObject, 0, .1f).setLoopPingPong(5).setOnComplete(() => isInvisablity = false);
         }
     }
 
@@ -38,7 +87,8 @@ public class PlayerControl : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Floor") || collision.gameObject.CompareTag("Blocker"))
         {
-            isJumped = false;
+            animator.SetBool("Jump", false);
+            isMidAir = false;
         }
     }
 }
